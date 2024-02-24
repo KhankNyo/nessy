a933de2adda4a0dccc11be7efbd00836076bd8bc
Khanh <ngokhanh809@gmail.com>
2024-02-23 22:37:43 -0800

    added u64 and i64

--- src/Include/Common.h
+++ src/Include/Common.h
@@ -1,13 +1,17 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdarg.h>
 #include <stdint.h>
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
+typedef uint64_t u64;
 typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
+typedef int64_t i64;
 typedef unsigned int uint;
 
 typedef u8 Bool8;
@@ -16,6 +20,7 @@ enum { false = 0, true };
 typedef struct SmallString {
     char Data[64];
 } SmallString;
+
 
 #define STATIC_ARRAY_SIZE(Arr) (sizeof(Arr) / sizeof((Arr)[0]))
 #define IN_RANGE(LowRange, n, HighRange) ((LowRange) <= (n) && (n) <= (HighRange))
