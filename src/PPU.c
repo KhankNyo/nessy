771ab8b8361f2c29d3e0d885299f04231f125558
Khanh <ngokhanh809@gmail.com>
2024-02-25 12:36:36 -0800

    made the ppu able to talk to the cartridge via a cartridge handle, and added a nes color palette

--- src/PPU.c
+++ src/PPU.c
@@ -1,7 +1,10 @@
 
 #include <stdlib.h>
 #include <time.h>
+
 #include "Common.h"
+#include "Nes.h"
+#include "Cartridge.h"
 
 
 typedef struct NESPPU NESPPU;
@@ -13,6 +16,11 @@ struct NESPPU
     int Scanline;
     u32 ScreenIndex;
 
+    uint v:15;
+    uint t:15;
+    uint x:3;
+    uint w:1;
+
     u8 PaletteColorIndex[0x20];
     u8 PatternTable[0x2000];
     u8 NameAndAttributeTable[0x400];
@@ -20,6 +28,7 @@ struct NESPPU
 
     NESPPU_FrameCompletionCallback FrameCompletionCallback;
     u32 *ScreenOutput;
+    NESCartridge **CartridgeHandle;
 };
 
 typedef enum NESPPU_CtrlReg 
@@ -34,11 +43,84 @@ typedef enum NESPPU_CtrlReg
     PPU_DATA,
 } NESPPU_CtrlReg;
 
-NESPPU NESPPU_Init(NESPPU_FrameCompletionCallback Callback, u32 BackBuffer[NES_SCREEN_BUFFER_SIZE])
+static u32 sPPURGBPalette[] = {
+    0x007C7C7C,
+    0x000000FC,
+    0x000000BC,
+    0x004428BC,
+    0x00940084,
+    0x00A80020,
+    0x00A81000,
+    0x00881400,
+    0x00503000,
+    0x00007800,
+    0x00006800,
+    0x00005800,
+    0x00004058,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00BCBCBC,
+    0x000078F8,
+    0x000058F8,
+    0x006844FC,
+    0x00D800CC,
+    0x00E40058,
+    0x00F83800,
+    0x00E45C10,
+    0x00AC7C00,
+    0x0000B800,
+    0x0000A800,
+    0x0000A844,
+    0x00008888,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00F8F8F8,
+    0x003CBCFC,
+    0x006888FC,
+    0x009878F8,
+    0x00F878F8,
+    0x00F85898,
+    0x00F87858,
+    0x00FCA044,
+    0x00F8B800,
+    0x00B8F818,
+    0x0058D854,
+    0x0058F898,
+    0x0000E8D8,
+    0x00787878,
+    0x00000000,
+    0x00000000,
+    0x00FCFCFC,
+    0x00A4E4FC,
+    0x00B8B8F8,
+    0x00D8B8F8,
+    0x00F8B8F8,
+    0x00F8A4C0,
+    0x00F0D0B0,
+    0x00FCE0A8,
+    0x00F8D878,
+    0x00D8F878,
+    0x00B8F8B8,
+    0x00B8F8D8,
+    0x0000FCFC,
+    0x00F8D8F8,
+    0x00000000,
+    0x00000000,
+};
+
+
+
+NESPPU NESPPU_Init(
+    NESPPU_FrameCompletionCallback Callback, 
+    u32 BackBuffer[NES_SCREEN_BUFFER_SIZE],
+    NESCartridge **CartridgeHandle)
 {
     NESPPU This = {
         .ScreenOutput = BackBuffer,
-        .FrameCompletionCallback = Callback
+        .FrameCompletionCallback = Callback,
+        .CartridgeHandle = CartridgeHandle,
     };
     srand(time(NULL));
     return This;
@@ -47,9 +129,8 @@ NESPPU NESPPU_Init(NESPPU_FrameCompletionCallback Callback, u32 BackBuffer[NES_S
 void NESPPU_StepClock(NESPPU *This)
 {
     /* does the render, a single pixel */
-    This->ScreenOutput[This->ScreenIndex] = rand() > RAND_MAX/2? 
-        0x00FFFFFF : 0x00000000l;
-
+    u32 LookupValue = rand() * STATIC_ARRAY_SIZE(sPPURGBPalette) / RAND_MAX;
+    This->ScreenOutput[This->ScreenIndex] = sPPURGBPalette[LookupValue];
     This->ScreenIndex++;
     if (This->ScreenIndex >= NES_SCREEN_BUFFER_SIZE)
         This->ScreenIndex = 0;
@@ -59,9 +140,9 @@ void NESPPU_StepClock(NESPPU *This)
     {
         This->Clk = 0;
         This->Scanline++;
-        if (This->Scanline >= 261)
+        if (This->Scanline >= 261) /* last scanline */
         {
-            This->Scanline = -1;
+            This->Scanline = -1; /* -1 to wrap around later */
             This->FrameCompletionCallback(This);
         }
     }
@@ -70,18 +151,21 @@ void NESPPU_StepClock(NESPPU *This)
 u8 NESPPU_ReadInternalMemory(NESPPU *This, u16 Address)
 {
     Address &= 0x3FFF;
-    if (Address < 0x2000) /* pattern table memory */
+    /* CHR ROM memory from the cartridge */
+    if (Address < 0x2000) 
     {
-        return This->PatternTable[Address];
+        if (!This->CartridgeHandle || !*This->CartridgeHandle)
+            return 0;
+
+        return NESCartridge_Read(*This->CartridgeHandle, Address);
     }
-    /* NOTE: manual said 0x2EFF
-     * https://www.nesdev.org/NESDoc.pdf */
-    else if (IN_RANGE(0x2000, Address, 0x2FFF)) /* name table and attribute table */
+    /* NOTE: PPU VRAM, but cartridge can also hijack these addresses through mappers */
+    else if (IN_RANGE(0x2000, Address, 0x3EFF)) 
     {
         Address %= 0x400;
         return This->NameAndAttributeTable[Address];
     }
-    else /* sprite palette, image palette */
+    else /* 0x3F00-0x3FFF: sprite palette, image palette */
     {
         Address %= 0x20;
         return This->PaletteColorIndex[Address];
@@ -91,18 +175,21 @@ u8 NESPPU_ReadInternalMemory(NESPPU *This, u16 Address)
 void NESPPU_WriteInternalMemory(NESPPU *This, u16 Address, u8 Byte)
 {
     Address &= 0x3FFF;
-    if (Address < 0x2000) /* pattern table memory */
+    /* CHR ROM memory from the cartridge */
+    if (Address < 0x2000) 
     {
-        This->PatternTable[Address] = Byte;
+        if (This->CartridgeHandle && *This->CartridgeHandle)
+        {
+            NESCartridge_Write(*This->CartridgeHandle, Address, Byte);
+        }
     }
-    /* NOTE: manual said 0x2EFF
-     * https://www.nesdev.org/NESDoc.pdf */
-    else if (IN_RANGE(0x2000, Address, 0x2FFF)) /* name table and attribute table */
+    /* NOTE: PPU VRAM, but cartridge can also hijack these addresses thorugh mappers */
+    else if (IN_RANGE(0x2000, Address, 0x3EFF)) 
     {
         Address %= 0x400;
         This->NameAndAttributeTable[Address] = Byte;
     }
-    else /* sprite palette, image palette */
+    else /* 0x3F00-0x3FFF: sprite palette, image palette */
     {
         Address %= 0x20;
         This->PaletteColorIndex[Address] = Byte;
